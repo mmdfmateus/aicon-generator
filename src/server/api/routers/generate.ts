36e9b7ec6b26ec0081f8bd53f11f1e6a2f377193
mmdfmateus <mmdfmateus@gmail.com>
2023-04-21 00:11:15 -0300

    add generate mutation

---
+++ src/server/api/routers/generate.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+import {
+    createTRPCRouter,
+    publicProcedure,
+    protectedProcedure,
+} from '~/server/api/trpc';
+
+export const generateRouter = createTRPCRouter({
+    generateIcon: publicProcedure
+        .input(
+            z.object({
+                prompt: z.string(),
+            })
+        )
+        .mutation(({ ctx, input }) => {
+            console.log('this actually works', input);
+            return {
+                message: 'success',
+                data: input.prompt,
+            };
+        }),
+});
