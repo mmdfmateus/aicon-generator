b3cb2891d9964ebbfd06f3ecaf75514348da10f5
mmdfmateus <mmdfmateus@gmail.com>
2023-04-21 00:11:26 -0300

    add generate page with simple form

---
+++ src/components/FormGroup.tsx
@@ -0,0 +1,7 @@
+
+
+export const FormGroup = (props: React.ComponentPropsWithoutRef<'div'>) => {
+    return <div className='flex flex-col gap-1' {...props}>
+        {props.children}
+    </div>;
+}
---
+++ src/components/Input.tsx
@@ -0,0 +1,4 @@
+
+export const Input = (props: React.ComponentPropsWithoutRef<'input'>) => {
+    return <input {...props} className='border border-gray-800 text-gray-800 px-4 py-2 rounded-lg'></input>
+}
