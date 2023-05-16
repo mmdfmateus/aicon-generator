e8acf3f01071fa75ab1b1fca7c97a665abcb6f71
mmdfmateus <mmdfmateus@gmail.com>
2023-05-16 00:15:00 -0300

    fixing build

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from '~/server/api/trpc';
-import { exampleRouter } from '~/server/api/routers/example';
 import { generateRouter } from './routers/generate';
 import { checkoutRouter } from './routers/checkout';
 
@@ -9,7 +8,6 @@ import { checkoutRouter } from './routers/checkout';
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-    example: exampleRouter,
     generate: generateRouter,
     checkout: checkoutRouter,
 });
